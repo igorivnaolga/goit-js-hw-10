dfb60f9a942c4c92ea0b85b78738ba851e25b456
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-09-28 08:59:40 +1300

    import SlimSelect styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
 
